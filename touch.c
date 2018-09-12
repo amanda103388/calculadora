c925e44c908d8f572d764df2706d6e0e953921cf
amanda103388 <ra103388@uem.br>
2018-09-12 16:45:51 -0300

    Main implementada

--- touch.c
+++ touch.c
@@ -17,5 +17,16 @@ double div(double x, double y){
 }
 
 int main(){
+	int x, y, n;
+	printf("digite um numero para x e y: ");
+	scanf("%d, %d", &x, &y);
+	printf("Digite uma operacao: \n
+		1-soma\n
+		2-subtracao\n
+		3-multiplicacao\n
+		4-divisao\n");
+	scanf("%i", &n);
+
+	switch(n)
 	return 0;
 }
